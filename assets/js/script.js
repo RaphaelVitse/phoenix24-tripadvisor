27727e0303f0e897f6bb5b0288085a677dcd284d
Raphael Vitse <raphaelv@MacBook-Pro-de-raphael.local>
2024-11-08 17:36:51 +0100

    Add function remove in modal js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   const switchModalOpening = document.querySelector("#btn-connect");
+  const switchModalClosing = document.querySelector(".close");
   const modal = document.querySelector(".modal");
 
   switchModalOpening.addEventListener("click", () => {
-    console.log("je clique");
-
+    console.log("je clique pour ouvrir");
     modal.classList.add("on");
+  });
+  switchModalClosing.addEventListener("click", () => {
+    console.log("je clique pour fermer");
+    modal.classList.remove("on").add(".hidden");
   });
 });
